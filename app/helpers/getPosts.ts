136e6142ab623bd0c0892980afee95993041c8ed
frendy.guo <frendy.guo@pinhome.id>
2022-01-19 13:40:14 +0700

    fix: remove posts dir from app

--- app/helpers/getPosts.ts
+++ app/helpers/getPosts.ts
@@ -14,6 +14,7 @@ type PostMarkdownAttributes = {
 const publicPath = path.resolve('.', 'public');
 
 export async function getPosts(): Promise<Post[]> {
+  console.log('READDIR', await fs.readdir(path.resolve('.')));
   const dir = await fs.readdir(path.join(publicPath, 'posts'));
   const posts = await Promise.all(
     dir.map(async filename => {
